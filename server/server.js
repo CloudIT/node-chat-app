513db27dfa707ed0f49e4864a4e769ce1dce5400
Jason Wagoner <cloudit.tv@gmail.com>
2018-05-28 15:53:22 -0400

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -13,7 +13,17 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New user connected')
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'Jason',
+    text: 'Hey, what is up?',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message)
+  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected from server');
