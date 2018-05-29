742fcae0460ff3542e9c333b35faf44cbcf7f0e0
Jason Wagoner <cloudit.tv@gmail.com>
2018-05-28 20:03:04 -0400

    Add geolocation support via geoloation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate the correct message object', () => {
@@ -10,5 +10,19 @@ describe('generateMessage', () => {
 
     expect(typeof message.createdAt).toBe('number');
     expect(message).toMatchObject({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Jason';
+    var lat = 38.8976763
+    var long = -77.0387185
+
+    var message = generateLocationMessage(from, lat, long);
+
+    expect(message.from).toBe(from);
+    expect(typeof message.createdAt).toBe('number');
+    expect(message.url).toBe(`https://www.google.com/maps?q=${lat},${long}`);
   });
 });
